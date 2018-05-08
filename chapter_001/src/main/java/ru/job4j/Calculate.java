eea678963ef3b47b27615db9f5d1f43ce464c409
PavelKuzenkov <paul_501@mail.ru>
2018-05-09 00:15:06 +0400

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
-package ru.job4j.calculate;
+package ru.job4j;
+
+/**
+* Class Calculate решение задачи части 001 урок 1.
+* @author Кузенков Павел
+* @since 08.05.2018
+*/
 
 public class Calculate{
+	
+	/**
+	* Конструктор, вывод строки в консоль.
+	* @param args - args.
+	*/
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
