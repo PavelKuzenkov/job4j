46ff1c73bd63c16039128070f93c2ab19ffc3765
PavelKuzenkov <paul_501@mail.ru>
2018-05-12 17:53:50 +0400

    3.1. Элементарный калькулятор. [#185]

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -7,20 +7,20 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Kuzenkov Pavel
-* @since 10.05.2018
-*/
+ * Test.
+ *
+ * @author Kuzenkov Pavel
+ * @since 10.05.2018
+ */
 public class CalculateTest {
 
 	/**
-	* Test echo.
-	*/ @Test
+	 * Test echo.
+	 */ @Test
 
 	public void whenTakeNameThenTreeEchoPlusName() {
 		String input = "Kuzenkov Pavel";
-		String expect = "Echo, echo, echo : Kuzenkov Pavel"; 
+		String expect = "Echo, echo, echo : Kuzenkov Pavel";
 		Calculate calc = new Calculate();
 		String result = calc.echo(input);
 		assertThat(result, is(expect));
