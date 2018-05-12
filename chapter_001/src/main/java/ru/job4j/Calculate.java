cb7396732065c94ce5fd8721a4131ba6fe6a85a7
PavelKuzenkov <paul_501@mail.ru>
2018-05-12 17:52:53 +0400

    3.1. Элементарный калькулятор. [#185]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,29 +1,29 @@
 package ru.job4j;
 
 /**
-* Class Calculate решение задачи части 001 урок 1.
-*
-* @author Кузенков Павел
-* @since 08.05.2018
-*/
+ * Class Calculate решение задачи части 001 урок 1.
+ *
+ * @author Кузенков Павел
+ * @since 08.05.2018
+ */
 
 public class Calculate {
-	
+
 	/**
-	* Конструктор, вывод строки в консоль.
-	* @param args - args.
-	*/
-	
+	 * Конструктор, вывод строки в консоль.
+	 * @param args args.
+	 */
+
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
-	
+
 	/**
-	* Method echo.
-	* @param name Your name.
-	* @return Echo plus your name.
-	*/
-	
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
+
 	public String echo(String name) {
 		return "Echo, echo, echo : " + name;
 	}
