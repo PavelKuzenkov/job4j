52499397a65bfa7650b2f627ef7831c7b83f18d5
PavelKuzenkov <paul_501@mail.ru>
2018-05-14 21:23:04 +0400

    5.0. Заполнить массив степенями чисел.

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+/**
+ * Class Paint решение задачи части 001 урок 6 5.0.
+ *
+ * @author Кузенков Павел.
+ * @since 14.05.2018
+ */
+
+public class Square {
+
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        int value = 1;
+        for (int i = 0; i != bound; i++) {
+            rst[i] = value * value;
+            value++;
+        }
+        return rst;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Kuzenkov Pavel
+ * @since 14.05.2018
+ */
+
+public class ArrayTest {
+
+    /**
+     * Test calculate bound 3.
+     */@Test
+    public void whenBound3Then149() {
+        int bound = 3;
+        Square square = new Square();
+        int[] result = square.calculate(bound);
+        int[] expect = new int[] {1, 4, 9};
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * Test calculate bound 5.
+     */@Test
+    public void whenBound5Then1491625() {
+        int bound = 5;
+        Square square = new Square();
+        int[] result = square.calculate(bound);
+        int[] expect = new int[] {1, 4, 9, 16, 25};
+        assertThat(result, is(expect));
+    }
+}
