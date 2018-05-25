ee5437a0d620fc9ac042461b8706c679aa44e624
PavelKuzenkov <paul_501@mail.ru>
2018-05-25 23:15:42 +0400

    1. Реализация профессий в коде

---
+++ chapter_002/src/main/java/ru/job4j/profession/House.java
@@ -0,0 +1,10 @@
+package ru.job4j.profession;
+
+/**
+ * Class House решение задачи части 002 урок 2 1.
+ *
+ * @author Кузенков Павел.
+ * @since 25.05.2018
+ */
+public class House {
+}
---
+++ chapter_002/src/main/java/ru/job4j/profession/Teacher.java
@@ -0,0 +1,17 @@
+package ru.job4j.profession;
+
+/**
+ * Class Teacher решение задачи части 002 урок 2 1.
+ *
+ * @author Кузенков Павел.
+ * @since 25.05.2018
+ */
+public class Teacher extends Profession{
+
+    /**
+     * Обучение.
+     */
+    public void teach() {
+
+    }
+}
