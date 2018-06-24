a6356447d7891d0584599bb3e235f2761ac5692c
PavelKuzenkov <paul_501@mail.ru>
2018-06-24 23:31:08 +0400

    2. Сортировка User с использованием Comparator

--- chapter_003/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sort/SortUser.java
@@ -1,8 +1,6 @@
 package ru.job4j.sort;
 
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * Class SortUser решение задачи части 003 урок 3 1.
@@ -21,5 +19,25 @@ public class SortUser {
         TreeSet<User> result = new TreeSet<User>(new Sort());
         result.addAll(list);
         return result;
+    }
+
+    /**
+     * Сортировка пользователей по длине имени.
+     * @param list Исходный список пользователей.
+     * @return Отсортированный список.
+     */
+    public List<User> sortNameLength(List<User> list) {
+        Collections.sort(list, new SortNameLength());
+        return list;
+    }
+
+    /**
+     * Сортировка пользователей по имени и возрасту.
+     * @param list Исходный список пользователей.
+     * @return Отсортированный список.
+     */
+    public List<User> sortByAllFields(List<User> list) {
+        Collections.sort(list, new SortNameAge());
+        return list;
     }
 }
--- chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
@@ -35,4 +35,61 @@ public class SortUserTest {
             assertThat(Integer.parseInt(user.getAge()), is(age += 5));
         }
     }
+
+    /**
+     * Test sortNameLength method.
+     */
+    @Test
+    public void when4UsersThenSortedByNameAge() {
+        SortUser sort = new SortUser();
+        List<User> list = new ArrayList<>();
+        list.add(new User("Vladimir", "25"));
+        list.add(new User("Lera", "5"));
+        list.add(new User("Yuriy", "20"));
+        list.add(new User("Arkadiy", "15"));
+        List<User> result = sort.sortNameLength(list);
+        assertThat(result.get(0).getName(), is("Lera"));
+        assertThat(result.get(1).getName(), is("Yuriy"));
+        assertThat(result.get(2).getName(), is("Arkadiy"));
+        assertThat(result.get(3).getName(), is("Vladimir"));
+    }
+
+    /**
+     * Test sortByAllFields method.
+     */
+    @Test
+    public void when5UsersThenSortedByNameLength() {
+        SortUser sort = new SortUser();
+        List<User> list = new ArrayList<>();
+        list.add(new User("Vladimir", "25"));
+        list.add(new User("Boris", "25"));
+        list.add(new User("Arkadiy", "20"));
+        list.add(new User("Arkadiy", "15"));
+        list.add(new User("Arkadiy", "30"));
+        list.add(new User("Boris", "15"));
+        list.add(new User("Arkadiy", "15"));
+        list.add(new User("Boris", "15"));
+        list.add(new User("Vladimir", "10"));
+        List<User> result = sort.sortByAllFields(list);
+        assertThat(result.get(0).getName(), is("Arkadiy"));
+        assertThat(result.get(0).getAge(), is("15"));
+        assertThat(result.get(1).getName(), is("Arkadiy"));
+        assertThat(result.get(1).getAge(), is("15"));
+        assertThat(result.get(2).getName(), is("Arkadiy"));
+        assertThat(result.get(2).getAge(), is("20"));
+        assertThat(result.get(3).getName(), is("Arkadiy"));
+        assertThat(result.get(3).getAge(), is("30"));
+        assertThat(result.get(4).getName(), is("Boris"));
+        assertThat(result.get(4).getAge(), is("15"));
+        assertThat(result.get(5).getName(), is("Boris"));
+        assertThat(result.get(5).getAge(), is("15"));
+        assertThat(result.get(6).getName(), is("Boris"));
+        assertThat(result.get(6).getAge(), is("25"));
+        assertThat(result.get(7).getName(), is("Vladimir"));
+        assertThat(result.get(7).getAge(), is("10"));
+        assertThat(result.get(8).getName(), is("Vladimir"));
+        assertThat(result.get(8).getAge(), is("25"));
+    }
+
+
 }
