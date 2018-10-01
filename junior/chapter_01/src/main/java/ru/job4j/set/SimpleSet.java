4d850981d2327de7473cd20a0184f0a6eaf3cad3
PavelKuzenkov <paul_501@mail.ru>
2018-10-01 18:00:29 +0400

    Реализовать коллекцию Set на массиве[#72863] композиция

--- junior/chapter_01/src/main/java/ru/job4j/set/SimpleSet.java
+++ junior/chapter_01/src/main/java/ru/job4j/set/SimpleSet.java
@@ -2,25 +2,56 @@ package ru.job4j.set;
 
 import ru.job4j.list.DynamicContainer;
 
+import java.util.Iterator;
+
 /**
  * Class SimpleSet.
  * @author Kuzenkov Pavel.
  * @since 26.09.2018.
  */
-public class SimpleSet<E> extends DynamicContainer<E> {
+public class SimpleSet<E> {
+
+    /**
+     * Data container.
+     */
+    private DynamicContainer<E> container = new DynamicContainer<>();
 
     /**
      * Overriding add method.
      * @param value new object.
      * @return "true" if added or "false" if not.
      */
-    @Override
     public boolean add(E value) {
         boolean result = false;
-        if (!super.has(value)) {
-            super.add(value);
+        if (!this.container.has(value)) {
+            this.container.add(value);
             result = true;
         }
         return result;
+    }
+
+    /**
+     * Getting the object in certain position.
+     * @param position position.
+     * @return object.
+     */
+    public E get(int position) {
+        return this.container.get(position);
+    }
+
+    /**
+     * Return size of source array.
+     * @return size.
+     */
+    public int size() {
+        return this.container.size();
+    }
+
+    /**
+     * Overriding iterator method.
+     * @return iterator.
+     */
+    public Iterator<E> iterator() {
+        return this.container.iterator();
     }
 }
---
+++ junior/chapter_01/src/test/java/ru/job4j/map/MyMapTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.map;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MyMapTest {
+
+    @Test
+    public void start() {
+        Map<User, String> map = new HashMap<>();
+        User user1 = new User("User1", 0, 20, 11, 1999);
+        User user2 = new User("User1", 0, 20, 11, 1999);
+        map.put(user1, "user1");
+        map.put(user2, "user2");
+        System.out.println(user1.equals(user2));
+        System.out.println(map);
+        Map<User, String> map2 = new HashMap<>();
+        map2.put(user1, "user1");
+        map2.put(user1, "user2");
+        System.out.println(map2);
+
+    }
+
+}
