b3b14e67a64cefbffe94a096bed9ee66873d295c
PavelKuzenkov <paul_501@mail.ru>
2018-10-04 03:23:13 +0400

    Создать модель User [#999] getters & setters

--- junior/chapter_01/src/main/java/ru/job4j/map/User.java
+++ junior/chapter_01/src/main/java/ru/job4j/map/User.java
@@ -38,4 +38,29 @@ public class User {
         this.children = amountOfChildren;
         this.birthday = new GregorianCalendar(year, month, day);
     }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
 }
